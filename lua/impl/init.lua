431ef7811f163b0f36ba1bef3f2520afaf226a3d
justinbather <justinbather@hotmail.com>
2025-10-05 21:55:42 +0100

    wait for lsp to attach, expose ImplRefresh user command

--- lua/impl/init.lua
+++ lua/impl/init.lua
@@ -1,12 +1,22 @@
 local M = {}
 
 function M.setup()
-	vim.api.nvim_create_autocmd({ "BufEnter", "TextChanged", "InsertLeave" }, {
-		pattern = "*.go",
-		callback = function()
-			require("impl.analyzer").refresh()
+	vim.api.nvim_create_autocmd("LspAttach", {
+		callback = function(args)
+			local client = vim.lsp.get_client_by_id(args.data.client_id)
+
+			-- Only for gopls
+			if client and client.name == "gopls" then
+				vim.schedule(function()
+					M.refresh()
+				end)
+			end
 		end,
 	})
+
+	vim.api.nvim_create_user_command("ImplRefresh", function()
+		M.refresh()
+	end, {})
 end
 
 return M
